f9af3b0f23557e49d1ba0ff439f4017872a55939
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-10 10:14:18 +0430

    Add exchangeRate table

--- database/migrations/2022_03_07_161924_create_exchangeRates_table.php
+++ database/migrations/2022_03_07_161924_create_exchangeRates_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('exchangeRates', function (Blueprint $table) {
             $table->id();
-            $table->string('name',30);
+            $table->integer('rate');
             $table->timestamps();
         });
     }
